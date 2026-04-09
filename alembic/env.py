98be640d0aec415435b4e1f66afff31c92fe2872
Nahid Hasan <nahidhasan9613@gmail.com>
2026-04-09 14:44:08 +0600

    fix: Correctly import Base and models in Alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -1,39 +1,29 @@
 import os
 import sys
 from logging.config import fileConfig
-# from unittest.mock import Base
-from app.db.base import Base
-from app.models.user import User
-from app.models.book import BookScan
-
-# Add the project root to the Python path
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.core.config import settings
+from app.db.base import Base  
+from app.models import User, BookScan, Transaction, Policy
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+if settings.DATABASE_URL:
+    sync_database_url = settings.DATABASE_URL.replace("+asyncpg", "")
+    config.set_main_option("sqlalchemy.url", sync_database_url)
+else:
+    pass
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
