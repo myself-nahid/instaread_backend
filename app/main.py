799221d47cf889fe9e147f4172bea63fd86ef44a
Nahid Hasan <nahidhasan9613@gmail.com>
2026-04-09 11:09:59 +0600

    update welcome message in root endpoint to reflect new branding

--- app/main.py
+++ app/main.py
@@ -32,4 +32,4 @@ app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the InstaRead AI Backend"}
+    return {"message": "Welcome to the QandelShield"}
